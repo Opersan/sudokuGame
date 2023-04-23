10a704ffafaef689c29682bf55fbed5dbbf35717
Furkan Kiraz <furkankirazmp@gmail.com>
2023-04-24 02:03:40 +0300

    a

--- Assets/Scripts/GridSquare.cs
+++ Assets/Scripts/GridSquare.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class GridSquare : MonoBehaviour
 {
+    public int columns = 0;
+    public int rows = 0;
+    public float square_offset = 0.0;
+    public GameObject grid_square;
+    public Vector2 start_position = new Vector2(0.0f, 0.0f);
+    public float square_scale = 1.0f;
+
+    private List<GameObject> grid_square = new List<GameObject>();
+
+
+
     // Start is called before the first frame update
     void Start()
     {
